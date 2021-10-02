e76e02b24feb805cdc3174f30c64132c77086051
Hugo Guerif <hugo.guerif@edu.esiee.fr>
2021-10-02 15:25:05 +0200

    Shots follow mouse

--- Assets/Scripts/GunFire.cs
+++ Assets/Scripts/GunFire.cs
@@ -9,7 +9,8 @@ public class GunFire : MonoBehaviour
     public float rateOfFire = 0.5f;
 	public float recoil = 0.5f;
 	public Rigidbody2D rbplayer;
-    private bool canShoot = true;
+	private bool canShoot = true;
+    
 
 	void Start(){
 		this.gameObject.GetComponentInParent<Rigidbody2D>();
@@ -23,8 +24,16 @@ public class GunFire : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
-    {
+	void FixedUpdate()
+	{
+		Vector3 mousePos = Input.mousePosition;
+		Vector2 direction = mousePos - transform.position;
+		Debug.Log(direction);
+		
+		float angle = Vector2.SignedAngle(Vector2.right, direction);
+		transform.eulerAngles = new Vector3 (0, 0, angle);
+		
+    	
         if (Input.GetButton("Fire1") && canShoot) 
         {
 	        StartCoroutine(Shoot());
