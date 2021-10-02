f63dfe8cd3017187377543262f0e408d11cbf4bc
napo-lazo <napoleon.lazo@hotmail.com>
2021-10-02 16:50:43 -0500

    Added simple chaos bar that fills as time passes

--- Assets/Scripts/gameManager.cs
+++ Assets/Scripts/gameManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameManager : MonoBehaviour
 {
@@ -9,16 +10,23 @@ public class gameManager : MonoBehaviour
 	int killingSpree;
 	int totalLife;
 	int currentLife;
+	float chaos;
 	
 	private Player player;
+	private Image chaosBarMask;
 	public GameObject playerPrefab;
 	public GameObject spawnPoint;
+	public Image chaosBar;
+	public float chaosFillSpeed = 0.01f;
 	
     void Start()
     {
 	    player = Instantiate(playerPrefab, spawnPoint.transform.position, spawnPoint.transform.rotation).GetComponent<Player>();
+	    chaosBarMask = chaosBar.transform.GetChild(0).GetComponent<Image>();
+	    print(chaosBarMask.transform.name);
 	    score = 0;
 	    combo = 0;
+	    chaos = 0.0f;
     }
 	
 	void enemyKilled(){
@@ -35,5 +43,7 @@ public class gameManager : MonoBehaviour
     void Update()
     {
 	    currentLife = player.getCurrentHealth();
+	    if (chaosBarMask.fillAmount < 1)
+	    	chaosBarMask.fillAmount += chaosFillSpeed * Time.deltaTime;
     }
 }
