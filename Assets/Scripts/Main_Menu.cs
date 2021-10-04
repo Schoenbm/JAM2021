57318d8811261806dd2628d0e078b422075d43de
Hugo Guerif <hugo.guerif@edu.esiee.fr>
2021-10-04 19:28:21 +0200

    Sound in Menu + Cursor InGame

--- Assets/Scripts/Main_Menu.cs
+++ Assets/Scripts/Main_Menu.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections;
+using UnityEngine.Audio;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class Main_Menu : MonoBehaviour
 {
+	public AudioManager audioManager;
+	
+	void Start(){
+		audioManager.Play("Menu_Theme");
+	}
+	
     public void Quit()
 	{
 		Application.Quit();
