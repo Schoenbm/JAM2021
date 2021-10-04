5fd8b361d37d43ba89a0a222af8fd4a5c4adcff5
Hugo Guerif <hugo.guerif@edu.esiee.fr>
2021-10-04 11:03:59 +0200

    Main_Menu scene
    Created a Main Menu scene.
    Not in final folder

---
+++ Assets/Scripts/Main_Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Main_Menu : MonoBehaviour
+{
+    public void Quit()
+	{
+		Application.Quit();
+	}
+	
+	public void Play(string name)
+	{
+		SceneManager.LoadScene(name);
+	}
+}
