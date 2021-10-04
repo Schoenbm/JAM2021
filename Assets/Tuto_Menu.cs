e1029950d532012792bb5568781e663b72c2e8d1
Hugo Guerif <hugo.guerif@edu.esiee.fr>
2021-10-04 19:38:43 +0200

    SFX bugs in menu

---
+++ Assets/Tuto_Menu.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine.Audio;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Tuto_Menu : MonoBehaviour
+{
+	public AudioManager audioManager;
+	
+	void Start(){
+		audioManager.Play("Menu_Theme");
+	}
+}
