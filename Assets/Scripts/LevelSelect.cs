08e21d432e04551ed241dca329d4c7b5540a7062
Ricardo Lozano <rickyme98@hotmail.com>
2021-10-04 00:15:48 -0500

    Level select implemented

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LevelSelect : MonoBehaviour
+{
+
+	public static string[] levels = {"Level1", "Level2", "Level3"};
+	
+	public void selectLevel(string sceneName)
+	{
+		SceneManager.LoadScene(sceneName);
+	}
+	
+	public void backToMenu() {
+		SceneManager.LoadScene("Menu");
+	}
+	
+	public void selectRandomLevel() {
+		int levelIndex = Random.RandomRange(0, levels.Length - 1);
+		SceneManager.LoadScene(levels[levelIndex]);
+	}
+	
+	
+}
